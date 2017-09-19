f86348767e1213e1917b3843ade891ce4fe4ef88
Bruno Silvano Costa <bruno_705@hotmail.com>
2017-09-19 00:32:21 -0300

    Node.js setup

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use('/', function (req, res) {
+    res.end('This is the root route.');
+})
+
+app.listen(port, function () {
+    console.log(`Server started at port ${port}`);
+})
