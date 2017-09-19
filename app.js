129bfb35f2cdc59f9443e1964b9f7728fa3dc9cd
Bruno Silvano Costa <bruno_705@hotmail.com>
2017-09-19 00:36:53 -0300

    added missing semicolons

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var port = process.env.PORT || 3000;
 
 app.use('/', function (req, res) {
     res.end('This is the root route.');
-})
+});
 
 app.listen(port, function () {
     console.log(`Server started at port ${port}`);
-})
+});
