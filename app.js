3b91a5734e69cce4fe5de2d415de03f5ee3a4e43
Bruno Silvano Costa <bruno_705@hotmail.com>
2017-10-03 20:54:46 -0300

    basic router setup

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.use('/', function (req, res) {
-    res.end('This is the root route.');
-});
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+    .get(function (req, res) {
+        var resJson = { name: 'Bruno' };
+        res.json(resJson);
+    });
+
+app.use('/api', bookRouter);
 
 app.listen(port, function () {
     console.log(`Server started at port ${port}`);
