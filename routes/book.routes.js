b6fe08d9d56e65d9e7c02cebbe57fe11fcbc4006
Bruno Silvano Costa <bruno_705@hotmail.com>
2017-10-08 19:09:11 -0300

    added book controller.

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -3,24 +3,11 @@ var router = require('express').Router();
 var bookRouter = router;
 
 var routes = function (Book) {
+    var bookController = require('.. /controllers/book.controller.js')(Book);
+
     bookRouter.route('/')
-        .post(function (req, res) {
-            var book = new Book(req.body);
-            book.save();
-            res.status(201).send(book);
-        })
-        .get(function (req, res) {
-            var query = {};
-            if (req.query.genre) {
-                query.genre = req.query.genre;
-            }
-            Book.find(query, function (err, books) {
-                if (err) res.status(500).send(err);
-                else {
-                    res.json(books);
-                }
-            });
-        });
+        .post(bookController.post)
+        .get(bookController.get);
 
     bookRouter.use('/:bookId', function (req, res, next) {
         Book.findById(req.params.bookId, function (err, book) {
